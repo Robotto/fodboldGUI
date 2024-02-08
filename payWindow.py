53bc7fe60cc0ba62254c40fdf91f0ed8ff20c3dc
Magnus H <mfh1106@gmail.com>
2024-02-08 14:00:43 +0100

    Fighting to get the dropdown/updating to work need some help

--- payWindow.py
+++ payWindow.py
@@ -13,13 +13,9 @@ class payWindowClass:
         Label(self.payWindow,
               text="Indbetal").pack()
 
-        self.money = Entry(self.payWindow)
-        self.money.pack()
-
-        self.button = Button(self.payWindow, text="betal", command= self.addMoney)
-        self.button.pack()
-
-    #Dropdown menu
+        Label(self.payWindow,
+              text="Vælg Medlem").pack()
+        #Dropdown menu
         # Dropdown menu options 
         options = list(self.master.fodboldtur.keys())
             
@@ -32,25 +28,32 @@ class payWindowClass:
         # Create Dropdown menu 
         drop = OptionMenu( self.payWindow , clicked , *options ) 
         drop.pack() 
-            
-        # Create Label 
-        label = Label( self.payWindow , text = " " ) 
-        label.pack() 
 
 
-    def addMoney(self, clicked, fodboldtur):
+        Label(self.payWindow,
+              text="Mængde").pack()
+
+        self.money = Entry(self.payWindow)
+        self.money.pack()
+
+        self.button = Button(self.payWindow, text="betal", command= self.addMoney)
+        self.button.pack()
+
+        selected = clicked.get
+
+    def addMoney(self, selected):
         try:
             amount = abs(int(self.money.get())) #HUSK AT VALIDERE INPUT!, kun positive heltal!
         except:
             messagebox.showerror(parent=self.payWindow , title="Beløb fejl!", message="Prøv igen.\nKun hele tal!")
             return
-        if clicked.get == 'Medlem':
+        # if (self.payWindow.clicked.get) == 'Medlem':
             messagebox.showerror(parent=self.payWindow, title="Vælg et Medlem", message="Du skal vælge et medlem\n før du kan indbetale")
-        else:
-            self.master.fodboldtur.update({clicked.get: (fodboldtur[clicked.get] + amount)})
-            self.master.progressLabelText.set(f"Indsamlet: {self.master.total} af {self.master.target} kroner:")
-            print(f"Indsamlet: {self.master.total} af {self.master.target} kroner!")
-            self.master.progress['value'] = self.master.total / self.master.target * 100
+        # else:
+        self.master.fodboldtur.update({selected: (self.master.fodboldtur[selected] + amount)})
+        self.master.progressLabelText.set(f"Indsamlet: {self.master.total} af {self.master.target} kroner:")
+        print(f"Indsamlet: {self.master.total} af {self.master.target} kroner!")
+        self.master.progress['value'] = self.master.total / self.master.target * 100
 
             ##TODO: TELL MAIN WINDOW TO PICKLE THE DICTIONARY
-            self.master.gemFilen()
+        self.master.gemFilen()
