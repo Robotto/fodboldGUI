0c1a0eaa85f7c8c142a0262dbe6f002d2fcffbbf
Magnus H <mfh1106@gmail.com>
2024-02-08 12:21:34 +0100

    Comments, -test functions

--- worstWindow.py
+++ worstWindow.py
@@ -7,32 +7,35 @@ test = False
 
 # Directory til betalinger.pk filen...
 fodboldtur ={"./assets/betalinger.pk"}
-
+# Åbner Betalinger.pk
 infile = open(filename,'rb')
 fodboldtur = pickle.load(infile)
 infile.close()
-
-if test is True:
-    print("Opened pk file")
 
 class worstWindowClass:
     def __init__(self, master):
         self.master = master #reference til main window objektet
         self.worstWindow = Toplevel(self.master.root)
         self.worstWindow.title("Bottom 3")
+        # Størrelse af geomatri i vinduet (størrelsen af vinduet)
         self.worstWindow.geometry("450x600")
+        # Hvilket vindue canvas'en sidder på, Størrelsen af Canvas og baggrunden
         canvas= Canvas(self.worstWindow, width= 450, height= 600, bg="SpringGreen2")
         Label(self.worstWindow, text="De værste betalere")
 
+        # Variabel til yCoordinate til brug i texten
         yCoord = 1
 
-        #TODO Fix pickle load / label merge
-        top = sorted(fodboldtur.items(), key=lambda item: item[1], reverse=True)
-        for pair in top[:3]:
-            canvas.create_text(150, (100 * yCoord), text=f"{pair[0]} har indbetalt {pair[1]}", fill="black", font=('Helvetica 15 bold'))
-            yCoord += 1
+        #TODONE Fix pickle load / label merge
 
-            if test is True:
-                print('Attempted to make text in the Canvas')
-        canvas.pack()
+        #Sorter listen/Filen, Vælger de nederste 3
+        top = sorted(fodboldtur.items(), key=lambda item: item[1])
+        # Danner par af Key og Value og tager de nederste 3
+        for pair in top[:3]:
+            # Laver et tekst objekt i canvas'en, Objekterne bliver spacet med 100px mellemrum
+            canvas.create_text(150, (100 * yCoord), text=f"{pair[0]} har indbetalt {pair[1]}", fill="black", font=('Helvetica 15 bold'))
+            
+            yCoord += 1 # Stepper (prep til næste run i for loopet)
+        
+        canvas.pack() #Pack'er canvasen på siden...
 
